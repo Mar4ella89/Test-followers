513450b7d48084fc89d21a550ab3cf223db20faa
Marat Baikin <3371335@gmail.com>
2023-06-02 23:39:35 +0300

    create favicon

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// import { Routes, Route } from 'react-router-dom';
+// import { lazy, Suspense } from 'react';
+
+// import PrivateRoute from 'modules/PrivateRoute/PrivateRoute';
+// import PublicRoute from 'modules/PublicRoute/PublicRoute';
+
+// const Navbar = lazy(() => import('./modules/Navbar/Navbar'));
+// const ContactBooks = lazy(() => import('./Pages/ContactBooks/ContactBooks'));
+// const LoginPage = lazy(() => import('./Pages/LoginPage/LoginPage'));
+// const RegisterPage = lazy(() => import('./Pages/RegisterPage/RegisterPage'));
+
+export const App = () => {
+  return (
+  <>HI</>
+    // <Suspense>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route element={<PrivateRoute />}>
+    //       <Route path="/contacts" element={<ContactBooks />} />
+    //     </Route>
+    //     <Route element={<PublicRoute />}>
+    //       <Route path="/login" element={<LoginPage />} />
+    //       <Route path="/register" element={<RegisterPage />} />
+    //     </Route>
+
+    //     <Route path="*" element={<LoginPage />} />
+    //   </Routes>
+    // </Suspense>
+  );
+};
