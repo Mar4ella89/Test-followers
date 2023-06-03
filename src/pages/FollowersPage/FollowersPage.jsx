5b5ee415591b1e752273f0a841b5174de4fcb761
Marat Baikin <3371335@gmail.com>
2023-06-04 02:40:37 +0300

    upd

--- src/pages/FollowersPage/FollowersPage.jsx
+++ src/pages/FollowersPage/FollowersPage.jsx
@@ -1,3 +1,7 @@
+import Container from 'components/Container/Container';
+
+import GoitIcon from 'images/icons/GoitIcon';
+
 import defaultImage from '../../images/Hansel.png';
 // import defaultImage from '../../images/213.jpg';
 
@@ -5,8 +9,11 @@ import css from './FollowersPage.module.css';
 
 const FollowersPage = () => {
   return (
-    <ul className={css.list}>
+    <Container>
+    <section className={css.container}>
+      <ul className={css.list}>
       <li className={css.item}>
+        <GoitIcon color={'#FFFFFF'} className={css.logo}/>
         <div className={css.middleLine}></div>
 
         <div className={css.avatarContainer}>
@@ -22,12 +29,39 @@ const FollowersPage = () => {
             />
           </div>
         </div>
+        <p className={css.userTitle}>Bob Dylan</p>
 
-        <p>777 tweets</p>
-        <p>100,501 Followers</p>
-        <button type="button">Following</button>
+        <p className={css.text}>777 tweets</p>
+        <p className={css.text}>100,501 Followers</p>
+        <button type="button" className={css.button}>Following</button>
+      </li>
+
+      <li className={css.item}>
+        <GoitIcon color={'#FFFFFF'} className={css.logo}/>
+        <div className={css.middleLine}></div>
+
+        <div className={css.avatarContainer}>
+          <div className={css.avatarBack}>
+            <img
+              src={defaultImage}
+              alt=""
+              //   alt={title}
+              loading="lazy"
+                className={css.image}
+              //   src={imageError ? defaultImage : imgUrl}
+              //   onError={handleImageError}
+            />
+          </div>
+        </div>
+        <p className={css.userTitle}>Bob Dylan</p>
+
+        <p className={css.text}>777 tweets</p>
+        <p className={css.text}>100,501 Followers</p>
+        <button type="button" className={css.button}>Following</button>
       </li>
     </ul>
+    </section>
+    </Container>
   );
 };
 
