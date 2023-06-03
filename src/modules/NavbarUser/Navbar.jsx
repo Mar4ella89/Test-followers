2e8f11b368a5a5ef42148dbc01b1441b5acaea54
Marat Baikin <3371335@gmail.com>
2023-06-03 14:48:08 +0300

    add img

--- src/modules/NavbarUser/Navbar.jsx
+++ src/modules/NavbarUser/Navbar.jsx
@@ -5,9 +5,15 @@ import css from './Navbar.module.css';
 const Navbar = () => {
   return (
     <div className={css.navList}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/tweets">Tweets</NavLink>
-      <NavLink to="/followers">Followers</NavLink>
+      <NavLink to="/" className={css.navLink}>
+        Home
+      </NavLink>
+      <NavLink to="/tweets" className={css.navLink}>
+        Tweets
+      </NavLink>
+      <NavLink to="/followers" className={css.navLink}>
+        Followers
+      </NavLink>
     </div>
   );
 };
--- src/pages/FollowersPage/FollowersPage.jsx
+++ src/pages/FollowersPage/FollowersPage.jsx
@@ -1,5 +1,22 @@
 const FollowersPage = () => {
-    return <>FollowersPage</>;
-}
+  return (
+    <ul>
+      <li>
+        <img
+          src="#"
+          alt=""
+          //   alt={title}
+          loading="lazy"
+          //   className={styles.image}
+          //   src={imageError ? defaultImage : imgUrl}
+          //   onError={handleImageError}
+        />
+        <p>777 tweets</p>
+        <p>100,501 Followers</p>
+        <button type="button">Following</button>
+      </li>
+    </ul>
+  );
+};
 
-export default FollowersPage
+export default FollowersPage;
