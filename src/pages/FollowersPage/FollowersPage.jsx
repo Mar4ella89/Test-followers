34961b81361a1d0c8f86a92a901d2ca81e0edb69
Marat Baikin <3371335@gmail.com>
2023-06-04 03:44:31 +0300

    upd

--- src/pages/FollowersPage/FollowersPage.jsx
+++ src/pages/FollowersPage/FollowersPage.jsx
@@ -10,8 +10,32 @@ import css from './FollowersPage.module.css';
 const FollowersPage = () => {
   return (
     <Container>
-    <section className={css.container}>
+    <section className={css.section}>
       <ul className={css.list}>
+      <li className={css.item}>
+        <GoitIcon color={'#FFFFFF'} className={css.logo}/>
+        <div className={css.middleLine}></div>
+
+        <div className={css.avatarContainer}>
+          <div className={css.avatarBack}>
+            <img
+              src={defaultImage}
+              alt=""
+              //   alt={title}
+              loading="lazy"
+                className={css.image}
+              //   src={imageError ? defaultImage : imgUrl}
+              //   onError={handleImageError}
+            />
+          </div>
+        </div>
+        <p className={css.userTitle}>Bob Dylan</p>
+
+        <p className={css.text}>777 tweets</p>
+        <p className={css.text}>100,501 Followers</p>
+        <button type="button" className={css.button}>Following</button>
+      </li>
+
       <li className={css.item}>
         <GoitIcon color={'#FFFFFF'} className={css.logo}/>
         <div className={css.middleLine}></div>
