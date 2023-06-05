2e5560177bda2b6a647697e7ae0a3024eb5d62f2
Marat Baikin <3371335@gmail.com>
2023-06-05 20:42:05 +0300

    upd

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,18 @@
+import Container from 'components/Container/Container';
+
+import css from './HomePage.module.css';
+
+import backgroundImage from '../../images/backphoto_1.png';
+
 const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <Container>
+      <section className={css.section}>
+        <img src={backgroundImage} alt="backgroundImage" className={css.img}/>
+        <p className={css.text}>HOME page coming soon...</p>
+      </section>
+      </Container>
+  );
 };
 
 export default HomePage;
