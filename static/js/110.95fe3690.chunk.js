1fb77c8de3d2debd1fed34c3695fbf3a20b20565
Mar4ella89 <103376023+Mar4ella89@users.noreply.github.com>
2023-06-05 18:26:09 +0000

    Deploying to gh-pages from @ Mar4ella89/test-tweets@150d59a957fa354d8856eb7277a0b36aa4664c1d 🚀

---
+++ static/js/110.95fe3690.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_tweets=self.webpackChunktest_tweets||[]).push([[110],{3110:function(e,s,t){t.r(s),t.d(s,{default:function(){return l}});var a=t(1087),n="Navbar_wrapper__Z0AWg",r="Navbar_navList__eeiZ-",c="Navbar_navLink__il1WQ",i=t(3329),l=function(){return(0,i.jsx)("div",{className:n,children:(0,i.jsxs)("div",{className:r,children:[(0,i.jsx)(a.OL,{to:"/",className:c,children:"Home"}),(0,i.jsx)(a.OL,{to:"/tweets",className:c,children:"Tweets"})]})})}}}]);
+//# sourceMappingURL=110.95fe3690.chunk.js.map
